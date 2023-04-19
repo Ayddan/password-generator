ec9bc6d491a46e1dc70531147bf9d41d18fd860c
Ayddan <nathanleduc@hotmail.fr>
2023-04-19 11:35:34 +0200

    Finish range slider

--- src/components/PasswordGenerator.vue
+++ src/components/PasswordGenerator.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
     import { ref } from 'vue'
-    const sliderValue = ref(50)
+    const sliderValue = ref(10)
 </script>
 
 <template>
@@ -20,10 +20,13 @@
     <div class="generator-settings">
         <div class="lenght-range-header">
             <div>Character Length</div>
-            <input v-model="sliderValue" class="length-value" type="text">
+            <input v-model="sliderValue" class="length-value" type="text" id="length-value" name="length-value">
         </div>
-        <input v-model="sliderValue" class="range" type="range" name="length" min="0" max="20">
-        <label class="ir" for="length">Character Length</label>
+        <div class="range-wrapper">
+            <div class="range-fill"></div>
+            <input v-model="sliderValue" class="range" type="range" name="length" min="0" max="20">
+            <label class="ir" for="length">Character Length</label>
+        </div>
     </div>
   </form>
 </template>
@@ -71,38 +74,128 @@
 .lenght-range-header{
     display: flex;
     justify-content: space-between;
+    margin-bottom: 20px;
 }
 
+.range-wrapper{
+    position: relative;
+    display: flex;
+    align-items: center;
+}
+
+.range-fill{
+    position: absolute;
+    top: calc(50% - 3px);
+    background-color: #A9FDB3;
+    height: 6px;
+    width: v-bind(sliderValue/20 * 100 + '%');
+    z-index: 0;
+}
+
+/* Hide div .range-fill for firefox */
+@-moz-document url-prefix() {
+    .range-fill{
+        display: none;
+    }
+}
+
+/* --- Range style --- */
+
+/* Chrome */
+
 input[type=range] {
-  height: 22px;
-  -webkit-appearance: none;
-  margin: 10px 0;
-  width: 100%;
-  background: none;
+    height: 22px;
+    -webkit-appearance: none;
+    margin: 0px 0;
+    width: 100%;
+    background: none;
 }
 input[type=range]:focus {
-  outline: none;
+    outline: none;
 }
 input[type=range].range::-webkit-slider-runnable-track {
-  width: 100%;
-  height: 5px;
-  cursor: pointer;
-  animate: 0.2s;
-  background: #14131B;
-  border-radius: 0;
-  border: none;
+    width: 100%;
+    height: 6px;
+    cursor: pointer;
+    animate: 0.2s;
+    background: #14131B;
+    border-radius: 0;
+    border: none;
 }
 input[type=range].range::-webkit-slider-thumb {
-  box-shadow: 0px 0px 0px 0px #A9FDB3;
-  border: none;
-  height: 20px;
-  width: 20px;
-  border-radius: 50%;
-  background: #E7E5EC;
-  cursor: pointer;
-  -webkit-appearance: none;
-  margin-top: -8px;
-  transition: box-shadow .3s ease;
+    position: relative;
+    box-shadow: 0px 0px 0px 0px #A9FDB3;
+    border: none;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background: #E7E5EC;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -8px;
+    transition: box-shadow .1s, background .1s;
+    z-index: 1;
+}
+
+input[type=range].range:focus::-webkit-slider-thumb {
+    position: relative;
+    box-shadow: 0px 0px 0px 2px #A9FDB3;
+    border: none;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background: #14131B;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -8px;
+    transition: box-shadow .3s,background .3s;
+    z-index: 1;
+}
+
+/* Firefox */
+
+input[type=range].range::-moz-range-track{
+    width: 100%;
+    height: 6px;
+    cursor: pointer;
+    animate: 0.2s;
+    background: #14131B;
+    border-radius: 0;
+    border: none;
+}
+
+input[type=range].range::-moz-range-thumb{
+    position: relative;
+    box-shadow: 0px 0px 0px 0px #A9FDB3;
+    border: none;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background: #E7E5EC;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -8px;
+    transition: box-shadow .1s, background .1s;
+    z-index: 1;
+}
+
+input[type=range].range:focus::-moz-range-thumb{
+    position: relative;
+    box-shadow: 0px 0px 0px 2px #A9FDB3;
+    border: none;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background: #14131B;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -8px;
+    z-index: 1;
+}
+
+input[type=range].range::-moz-range-progress {
+  height: 4px;
+  background: #A9FDB3;
 }
 
 </style>
